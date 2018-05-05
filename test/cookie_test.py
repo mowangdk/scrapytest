cc878dfeab6ee420e407473863cf21e457114d92
geyingqi <geyingqi@kuailexue.com>
2018-05-05 18:14:41 +0800

    add basic login for yikeitem

--- test/cookie_test.py
+++ test/cookie_test.py
@@ -28,7 +28,8 @@ def save_cookie_tofile():
     handler = urllib2.HTTPCookieProcessor(cookie)
     # 通过handler来构建opener
     opener = urllib2.build_opener(handler)
-    response = opener.open("http://www.baidu.com")
+    response = opener.open("https://passport.ekwing.com/index/login?callback=jQuery172028761594192983286_1525513573276&uname=78240037&pw=pjGwaDtMogr0lrFNPjmE9i7wiVGq4P1%2B0iiRVtrx6bvLK1YSvoth1LOGmxSAuGow3qcYP7HZZZ3E1ToVqvus0OVhLHb8orlGMTCNtdmd8YW6RUSKxOkY7516CQX2KrzuIpKrzZXtrMxN8HYFBBzEEnCX1hvgVDttpI8OBDrJmbI%3D&client_type=web&encrypt_key=3ffb3a92f1c4d3a388d9e00e707636a3&encrypt_type=rsa&utype=1&mem_type=2&_=1525513788306")
+    print response.read()
     # ignore_discard: save even cookies set to b discard, 即使cookies将被丢弃也将它保存下来
     # save even cookies that have expired the file is over written if it already exists, 如果该文件中cookies已经存在， 则覆盖源文件写入，
     cookie.save(ignore_discard=True, ignore_expires=True)
@@ -39,10 +40,12 @@ def read_cookie_fromfile():
     # 从文件中读取cookie内容到变量
     cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
     # 创建请求的request
-    req = urllib2.Request("http://www.baidu.com")
+    req = urllib2.Request("https://www.ekwing.com/exam/special/papergenerate")
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
     response = opener.open(req)
+    print response.read()
 
 
 if __name__ == '__main__':
-    save_cookie_tofile()
+    # save_cookie_tofile()
+    read_cookie_fromfile()
--- yikeitemgrep/yikeitemgrep/items.py
+++ yikeitemgrep/yikeitemgrep/items.py
@@ -8,7 +8,16 @@
 import scrapy
 
 
-class YikeitemgrepItem(scrapy.Item):
+class PaperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    paper_id = scrapy.Field()
+    paper_title = scrapy.Field()
+
+
+if __name__ == '__main__':
+    paper = PaperItem(paper_id='12345')
+    print id(paper)
+    paper2 = PaperItem(paper)
+    print id(paper2)
+
